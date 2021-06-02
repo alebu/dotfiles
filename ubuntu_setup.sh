8fb1247e7a4b8ae29a64a8a6067caf02722e52b0
Alessio Burani <alessio.burani@gmail.com>
2021-06-02 11:59:14 +0100

    updated ubuntu setup

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-
+echo updating
 sudo apt update
 
 # Vim
+echo installing Vim
 sudo apt install vim
 ln -s ~/projects/dotfiles/vim/.vimrc ~/.vimrc
 
 # NordVPN
+echo installing NordVPN
 sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
 
 # Oh-my-zsh
+echo installing zsh
+sudo apt install zsh
 echo installing oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo linking custom .zshrc file
@@ -26,15 +30,17 @@ rm ~/.tmux.conf
 ln -s ~/projects/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 # R
-
+echo installing R
+sudo apt install r-base
 # Python
+echo installing pyenv/pipenv
 sudo apt install pyenv
 pip install -U pipenv
 
 # utilities
+echo installing htop
 sudo apt install htop
 
 # LaTex
-
-# VSCode
-
+echo installing LaTex
+sudo apt-get install texlive-full
